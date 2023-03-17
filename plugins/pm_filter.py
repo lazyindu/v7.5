11b0832bf4c48f0ea3df1d727b62d6539f1720f0
indu <70087467+lazyindu@users.noreply.github.com>
2023-03-17 20:51:47 +0530

    Recent Updates

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -943,7 +943,7 @@ async def auto_filter(client, msg, spoll=False):
             else:
                 btn = [
                 [
-                    InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start={pre}#{file.file_id}")),
+                    InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{pre}#{file.file_id}")),
                 ]
                 for file in files
                 ]
@@ -980,11 +980,11 @@ async def auto_filter(client, msg, spoll=False):
                 [
                     InlineKeyboardButton(
                         text=f"[{get_size(file.file_size)}] {file.file_name}", 
-                        url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start={pre}#{file.file_id}")
+                        url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{pre}#{file.file_id}")
                     ),
                     InlineKeyboardButton(
                         text=f"[{get_size(file.file_size)}] {file.file_name}", 
-                        url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start={pre}#{file.file_id}")
+                        url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{pre}#{file.file_id}")
                     ),
                 ]
                 for file in files
