6c05202939052db831fe999fb565e096d126ec77
indu <70087467+lazyindu@users.noreply.github.com>
2023-03-19 22:31:15 +0530

    Add files via upload

--- ai_LazyDeveloper.py
+++ ai_LazyDeveloper.py
@@ -1,28 +1,30 @@
 import openai
-from telegram.ext import Updater, MessageHandler, Filters
-from info import *
+import os
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(token = "5942641741:AAFyOtC2NguQFnolhE8eFQF1D2TzOabwevo")
+dp = Dispatcher(bot)
 openai.api_key = "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"
 
-def generate_response(message):
-    prompt = f"User: {message}\nBot:"
+@dp.message_handler(commands = ['lazyai'])
+async def welcome(message: types.Message):
+    await message.reply("Hello am chat Gpt feature of lazyprincess")
+
+@dp.message_handler()
+async def gpt(message: types.message):
     response = openai.Completion.create(
-        engine="text-davinci-004",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=60,
-        n=1,
-        stop=None,
-        frequency_penalty=0,
-        presence_penalty=0
+        model = "text-davinci-003",
+        prompt = message.text,
+        temprature=0.5,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0.1,
+        presence_penalty=0.0,
     )
-    return response.choices[0].text.strip()
+    await message.reply(response.choices[0].text)
 
-def handle_message(update, context):
-    message = update.message.text
-    response = generate_response(message)
-    update.message.reply_text(response)
+if __name__ == "__main__":
+    executor.start_polling(dp)
+    
 
-updater = Updater(f"{BOT_TOKEN}", use_context=True)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
-updater.start_polling()
+
