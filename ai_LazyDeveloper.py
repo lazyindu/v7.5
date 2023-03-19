2b79e87ce1c1460add65a3dc59ac0d6549a6dcd6
indu <70087467+lazyindu@users.noreply.github.com>
2023-03-19 23:40:42 +0530

    Update ai_LazyDeveloper.py

--- ai_LazyDeveloper.py
+++ ai_LazyDeveloper.py
@@ -6,7 +6,7 @@ bot = Bot(token = "5942641741:AAFyOtC2NguQFnolhE8eFQF1D2TzOabwevo")
 dp = Dispatcher(bot)
 openai.api_key = "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"
 
-@dp.message_handler(commands = ['lazyai'])
+@dp.message_handler(commands = ['/lazyai'])
 async def welcome(message: types.Message):
     await message.reply("Hello am chat Gpt feature of lazyprincess")
 
@@ -15,7 +15,7 @@ async def gpt(message: types.message):
     response = openai.Completion.create(
         model = "text-davinci-003",
         prompt = message.text,
-        temprature=0.5,
+        temperature=0.5,
         max_tokens=1024,
         top_p=1,
         frequency_penalty=0.1,
